1863f00b86ae910a7d21952998f2c5cc98e23b4e
Faisal <faisaladamj@gmail.com>
2024-05-24 20:49:38 -0400

    added await to async func

--- commands/music_commands/billboardQueue.js
+++ commands/music_commands/billboardQueue.js
@@ -88,7 +88,7 @@ module.exports = {
             await entry.getTask();
         
             // add track(s) (this will add playlist or single track from the result)
-            queue.addTrack(result.tracks[0]);
+            queue.addTrack(result.tracks[0]);   
 
             try {
                 // if player node was not previously playing, play a song
